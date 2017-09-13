391e2de0d75384d571117580a4073e9b2786f8b1
scollet <origamillamaa@gmail.com>
2017-09-13 12:47:23 -0700

    h

--- js/exercisetracker.js
+++ js/exercisetracker.js
@@ -1,1 +1,21 @@
-//
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   exercisetracker.js                                 :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: scollet <marvin@42.fr>                     +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2017/09/13 12:46:04 by scollet           #+#    #+#             //
+//   Updated: 2017/09/13 12:46:07 by scollet          ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+document.addEventListener("deviceready", function(){
+
+  if(navigator.network.connection.type == Connection.NONE){
+    $("#home_network_button").text('No Internet Access')
+                 .attr("data-icon", "delete")
+                 .button('refresh');
+  }
+
+});
