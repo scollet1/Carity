f0d028fd841fdf93bcbcc49bb6ac76b7564712ef
scollet <origamillamaa@gmail.com>
2017-09-13 15:00:08 -0700

    la

--- js/exercisetracker.js
+++ js/exercisetracker.js
@@ -14,6 +14,23 @@ var track_id = '';      // Name/ID of the exercise
 var watch_id = null;    // ID of the geolocation
 var tracking_data = []; // Array containing GPS position objects
 
+// Initialize the default app
+firebase.initializeApp(defaultAppConfig);
+
+// Initialize another app with a different config
+var otherApp = firebase.initializeApp(otherAppConfig, "other");
+
+console.log(firebase.app().name);  // "[DEFAULT]"
+console.log(otherApp.name);        // "other"
+
+// Use the shorthand notation to retrieve the default app's services
+var defaultStorage = firebase.storage();
+var defaultDatabase = firebase.database();
+
+// Use the otherApp variable to retrieve the other app's services
+var otherStorage = otherApp.storage();
+var otherDatabase = otherApp.database();
+
 $("#startTracking_start").live('click', function(){
 
     // Start tracking the User
